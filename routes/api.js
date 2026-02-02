58f90ac061746985a3adb7df891772bcec7628c8
yudzxyz <yydzstore@gmail.com>
2026-02-03 00:49:09 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -6,13 +6,9 @@ const method = require('../methods')
 const apiHandle = async (ctx) => {
   const methodWithExt = ctx.params[0].match(/(.*).(png|webp)/)
   if (methodWithExt) ctx.props.ext = methodWithExt[2]
-  const raw = methodWithExt ? methodWithExt[1] : ctx.params[0]
-  console.log('RAW PARAM:', ctx.params[0])
-  const name = raw.split('/').pop()
-  ctx.result = await method(name, ctx.props)
-
+  ctx.result = await method(methodWithExt ? methodWithExt[1] : ctx.params[0], ctx.props)
 }
 
-api.post('/*', apiHandle)
+api.post('/', apiHandle)
 
 module.exports = api
